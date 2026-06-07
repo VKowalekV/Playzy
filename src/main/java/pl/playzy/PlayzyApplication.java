ab54edde5588f0b34d0f759e7cbfc96155aa0af7
VKowalekV <vkowalekv@gmail.com>
2026-06-07 14:13:47 +0200

    init

---
+++ src/main/java/pl/playzy/PlayzyApplication.java
@@ -0,0 +1,13 @@
+package pl.playzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayzyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayzyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/playzy/PlayzyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.playzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayzyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
